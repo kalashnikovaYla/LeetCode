d800aca769c3b9821f15f0cb72d22003f1736c6a
User <user@Admins-MacBook-Pro.local>
2025-04-08 16:30:57 +0500

    28

--- 28_FindtheIndexoftheFirstOccurrenceinString.playground/Contents.swift
+++ 28_FindtheIndexoftheFirstOccurrenceinString.playground/Contents.swift
@@ -9,6 +9,7 @@
  Output: 0
  Explanation: "sad" occurs at index 0 and 6.
  The first occurrence is at index 0, so we return 0.
+ 
  Example 2:
 
  Input: haystack = "leetcode", needle = "leeto"
@@ -40,32 +41,32 @@ class Solution {
 
 
 func strStr(_ haystack: String, _ needle: String) -> Int {
-       var needleCount = needle.count
-       var hayArr = Array(haystack)
-       var needleArr = Array(needle)
-       
-       if needleCount > hayArr.count {
-           return -1
-       }
-       for i in 0..<hayArr.count {
-           var finalCount:Int = 0
-           if hayArr[i] == needle.first {
-               var incCount = 0
-               for j in 0..<needleArr.count {
-                   if incCount + i >= hayArr.count {
-                       return -1
-                   }
-                   if needleArr[j] == hayArr[incCount + i] {
-                       finalCount += 1
-                       if finalCount == needleCount {
-                           return i
-                       }
-                   }
-                   incCount += 1
-               }
-           }
-       }
-       
-       
-       return -1
-   }
+    var needleCount = needle.count
+    var hayArr = Array(haystack)
+    var needleArr = Array(needle)
+    
+    if needleCount > hayArr.count {
+        return -1
+    }
+    for i in 0..<hayArr.count {
+        var finalCount:Int = 0
+        if hayArr[i] == needle.first {
+            var incCount = 0
+            for j in 0..<needleArr.count {
+                if incCount + i >= hayArr.count {
+                    return -1
+                }
+                if needleArr[j] == hayArr[incCount + i] {
+                    finalCount += 1
+                    if finalCount == needleCount {
+                        return i
+                    }
+                }
+                incCount += 1
+            }
+        }
+    }
+    
+    
+    return -1
+}
